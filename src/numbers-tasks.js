8262373586771693b01c77830762d9d99740af7c
Svetlana <brigantina@mail.ru>
2025-09-02 21:17:17 +0300

    feat: fixed error

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -333,8 +333,13 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  if (num < 1) return false;
+  while (num > 1) {
+    if (num % 2 !== 0) return false;
+    num /= 2;
+  }
+  return true;
 }
 
 /**
