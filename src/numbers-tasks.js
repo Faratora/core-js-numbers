9b2f8c408262392bb0d801df653fa9b286775758
Svetlana <brigantina@mail.ru>
2025-09-17 13:09:06 +0300

    feat: solution task 14

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -255,7 +255,7 @@ function getCube(num) {
 }
 
 /**
- * Returns the Fibonacci number located at the index position.
+ * 14. Returns the Fibonacci number located at the index position.
  *
  * @param {number} index
  * @return {number}
@@ -267,8 +267,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= index; i += 1) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 }
 
 /**
