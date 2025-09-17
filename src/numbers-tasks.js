2159fab5da135060dee9930b7452580151057ccb
Svetlana <brigantina@mail.ru>
2025-09-17 13:10:33 +0300

    feat: solution task 15

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -282,7 +282,7 @@ function getFibonacciNumber(index) {
 }
 
 /**
- * Returns the sum of all numbers from 1 to n.
+ * 15. Returns the sum of all numbers from 1 to n.
  *
  * @param {number} n
  * @return {number}
@@ -292,8 +292,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
