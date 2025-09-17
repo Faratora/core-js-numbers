129acec7580e86279bce4a0885d010c5fa8c6099
Svetlana <brigantina@mail.ru>
2025-09-17 11:43:47 +0300

    feat: solution task 9

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -147,7 +147,7 @@ function parseNumberFromString(value) {
 }
 
 /**
- * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
+ * 9. Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
  *
  * @param {number} a
  * @param {number} b
@@ -159,8 +159,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
